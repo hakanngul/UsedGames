c4c68029c8fa03312c13751d00e098cf8ca65cbd
hhakangull <hakanngul@icloud.com>
2023-03-23 23:20:56 +0300

    Initial Commit

---
+++ UsedGames/UsedGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesApp.swift
+//  UsedGames
+//
+//  Created by Hakan Gül on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
